34caa15b2dbe074a362af037d25e278ad6539f6c
username <lara.viquiera@gmail.com>
2023-12-17 23:44:12 -0800

    added event listeners and functions

--- js/main.js
+++ js/main.js
@@ -22,8 +22,34 @@
 
 	/*----- event listeners -----*/
 
+	keys.forEach(key => {
+		key.addEventListener('click', () => {
+		  const selectedLetter = key.textContent;
+		  handleLetterSelection(selectedLetter);
+		});
+	  });
+
 
 	/*----- functions -----*/
+	function handleLetterSelection(selectedLetter) {
+		const isCorrectGuess = currentWord.includes(selectedLetter);
+	  
+		keys.forEach(key => {
+		  if (key.textContent === selectedLetter) {
+			if (isCorrectGuess) {
+			  key.classList.add('correct-guess');
+			  updateWordDisplay(selectedLetter)
+			} else {
+			  key.classList.add('incorrect-guess');
+			  decreaseLives();
+			}
+			key.disabled = true;
+		  }
+		});
+	  
+	  }
+
+	  
 	init();
 
 	function () {
