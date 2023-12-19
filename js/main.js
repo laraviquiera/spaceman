12804a01b98a9ed87441063b5eac32919f973a1a
username <lara.viquiera@gmail.com>
2023-12-18 16:18:10 -0800

    organized functions and elements

--- js/main.js
+++ js/main.js
@@ -1,56 +1,79 @@
-/*----- state variables -----*/
-let numLives = 6;
-let currentWord;
-let mistakes = 0;
-let guessed = [];
-
-
 /*----- cached elements  -----*/
+
+const wordDisplay = document.querySelector('.word-display');
+const livesDisplay = document.querySelector('.num-lives b');
 const keys = document.querySelectorAll('.key');
-const wordDisplay = document.getElementsByClassName('word-display');
-const livesDisplay = document.querySelector('.num-lives b');	
-const randomWord = () => {
-	const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
-	currentWord = word;
-	document.getElementsByClass("hint-text b").innerText = hint;
-	wordDisplay.innerHTML = word.split("").map(() => `<li class="letter"></li>`).join("");
-	numLives = 6;
-	displayLives();
-	};
+const gameResult = document.querySelector('.game-result');
+const playAgainBtn = gameResult.querySelector('button');
+
+/*----- app's state (variables) -----*/
+
+let currentWord;
+let guessedLetters;
+let wrongGuesses;
+let numLives;
 
 /*----- event listeners -----*/
 
-keys.forEach(key => {
-	key.addEventListener('click', () => {
-		const selectedLetter = key.textContent;
-		handleLetterSelection(selectedLetter);
-	});
-});
 
 
 /*----- functions -----*/
-function handleLetterSelection(selectedLetter) {
-	const isCorrectGuess = currentWord.includes(selectedLetter);
-	
-	keys.forEach(key => {
-		if (key.textContent === selectedLetter) {
-			if (isCorrectGuess) {
-				key.classList.add('correct-guess');
-				updateWordDisplay(selectedLetter)
-			} else {
-				key.classList.add('incorrect-guess');
-				decreaseLives();
-			}
-			key.disabled = true;
-		  }
-		});
-	  
-	  }
 
-	
 init();
 
-function () {
-
-render();
+function init() {
+  currentWord = "";
+  guessedLetters = "";
+  wrongGuesses = 0;
+  numLives = 6;
+  render();
 }
+
+
+function getRandomWord() {
+	const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
+	document.querySelector(".hint-text b").innerText = hint;
+	wordDisplay.innerHTML = word.split("").map(() => `<li class="letter"></li>`).join("");
+	render();
+  };
+
+//Virtual Keyboard
+function keyboardLetters() {
+	keys.forEach(key => {
+	key.addEventListener('click', () => {
+	  guessedLetters = key.textContent;
+	  handleLetterSelection(guessedLetters);
+	});
+  });
+	render();
+  }
+
+function startGame() {
+	const initGame = (button, clickedLetter) => {
+	  if(currentWord.includes(clickedLetter)) {
+		[...currentWord].forEach((letter, index) => {
+		  if(letter === clickedLetter) {
+			guessed.push(letter);
+			wordDisplay.querySelectorAll("li")[index].innerText = letter;
+			wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
+		  }
+		});
+		} else {
+			numLives--;
+		  }
+	  
+		button.disabled = true; // Disabling the clicked button so user can't click again
+		livesDisplay.innerText = `${numLives}`;
+		// Calling gameOver function if any of these condition meets
+		if(wrongGuesses === numLives) return gameOver(false);
+		if(guessedLetters.length === currentWord.length) return gameOver(true);
+	  }
+  }
+  
+  
+  
+  function render() {
+	// guessedLetters();
+	// wrongGuesses();
+	// numLives();
+  }
